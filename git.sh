62ee28862e0c4fe389c2b5ddc62dd03d68561727
kartickjangoo88 <kartick@jangoo.us>
2023-09-03 17:10:47 +0530

    Added all codes

--- git.sh
+++ git.sh
@@ -2,10 +2,17 @@
 
 git add .
 
+echo
 echo "Added all codes"
+echo
 
 git commit -a -m "Added all codes"
+
+echo
 echo "Commited successfully"
+echo
 
 git push
+echo
 echo "pushed on git, congrats!!"
+echo
